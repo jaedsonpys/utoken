6c13247a0dad50648ba9a4753c57729814670023
Jaedson Silva <contacanal798@gmail.com>
2022-02-14 19:46:26 -0300

    add: adicionando autor, email do autor e descriçao em setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@ from setuptools import setup
 
 setup(
     name='utoken',
+    author='Jaedson Silva',
+    author_email='imunknowuser@protonmail.com',
+    description='Crie tokens de autenticação íntegros e seguros com UToken.'
     version='1.0.0',
     packages=['utoken'],
     project_urls={
