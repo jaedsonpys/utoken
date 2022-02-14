2f0966f95c74df33659e833be246e2d627c8644f
Jaedson Silva <contacanal798@gmail.com>
2022-02-14 19:44:27 -0300

    add: criando arquivo "setup.py"

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='utoken',
+    version='1.0.0',
+    packages=['utoken'],
+    project_urls={
+        'Código fonte': 'https://github.com/jaedsonpys/utoken'
+    },
+    license='GNU GPL v3.0'
+)
