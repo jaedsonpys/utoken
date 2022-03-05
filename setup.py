09e29bb5be4eb7d18f8727b5616cf1640e8b087d
Jaedson Silva <contacanal798@gmail.com>
2022-03-05 13:19:44 -0300

    setup: atualizando versao do projeto

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     author='Jaedson Silva',
     author_email='imunknowuser@protonmail.com',
     description='Crie tokens de autenticação íntegros e seguros com UTokeniz.',
-    version='1.0.0',
+    version='1.1.0',
     packages=['utoken'],
     project_urls={
         'Código fonte': 'https://github.com/jaedsonpys/utoken'
