55cb775da4c6db6e7e7abc29383bcc65bc711f82
Jaedson Silva <contacanal798@gmail.com>
2023-01-15 03:35:48 -0300

    update utoken examples

--- examples/simple_use.py
+++ examples/simple_use.py
@@ -1,9 +1,8 @@
-from utoken import encode, decode
-from os import urandom
+import utoken
 
 KEY = 'secret-key'
-my_token = encode({'message': 'Firlast'}, KEY)
-my_decoded_token = decode(my_token, KEY)
+my_token = utoken.encode({'message': 'Firlast'}, KEY)
+payload = utoken.decode(my_token, KEY)
 
 print(my_token)
-print(my_decoded_token)
+print(payload)
--- examples/simple_use_with_time.py
+++ examples/simple_use_with_time.py
@@ -1,25 +1,19 @@
-from utoken import encode, decode
-from utoken import ExpiredTokenError
-
-from datetime import datetime
+from time import sleep
 from datetime import timedelta
 
-from time import sleep
+import utoken
 
 KEY = 'secret-key'
 
-# Em max_time definimos que o token expira
-# daqui a 5 segundos.
-
-max_time = datetime.now() + timedelta(seconds=5)
-my_token = encode({'message': 'Firlast', 'max-time': max_time}, KEY)
-
+# In "expires_in" we set token timeout of 5 seconds
+my_token = utoken.encode({'name': 'Maria'}, KEY, expires_in=timedelta(seconds=5))
 print(my_token)
+
 sleep(6)
 
 try:
-    my_decoded_token = decode(my_token, KEY)
-except ExpiredTokenError:
+    payload = utoken.decode(my_token, KEY)
+except utoken.exceptions.ExpiredTokenError:
     print('O token está expirado.')
 else:
-    print(f'Conteúdo do token: {my_decoded_token}')
+    print(f'Conteúdo do token: {payload}')
